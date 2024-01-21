be5175661f5da2e44860c99d0fa58df849f860e2
Josan Behrends <josanbehrends@gmail.com>
2024-01-21 18:12:35 -0300

    Lançamento do Projeto com Rodape e Cabecalho prontos

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,11 @@
+import styles from './Rodape.module.css'
+
+function Rodape() {
+    return (
+        <footer className={styles.rodape}>
+            <h2>Desenvolvido por Josan Behrends durante o curso da Alura.</h2>
+        </footer>
+    )
+}
+
+export default Rodape
